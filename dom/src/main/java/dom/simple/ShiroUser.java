1635818cf2fd975ef5519d653751e67684f0e9fd
leanddrot <leanddro@hotmail.com>
2014-08-31 22:37:14 -0300

    agregado metodo init() a ShiroUser

--- dom/src/main/java/dom/simple/ShiroUser.java
+++ dom/src/main/java/dom/simple/ShiroUser.java
@@ -3,6 +3,7 @@ package dom.simple;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
+import javax.annotation.PostConstruct;
 import javax.jdo.annotations.Column;
 import javax.jdo.annotations.Element;
 import javax.jdo.annotations.IdentityType;
@@ -15,20 +16,17 @@ import org.apache.isis.applib.annotation.Bounded;
 import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.Named;
 import org.apache.isis.applib.annotation.ObjectType;
+import org.apache.isis.applib.annotation.Programmatic;
 import org.apache.isis.applib.annotation.Render;
 import org.apache.isis.applib.annotation.Title;
 
-@javax.jdo.annotations.PersistenceCapable(identityType=IdentityType.DATASTORE)
-@javax.jdo.annotations.DatastoreIdentity(
-        strategy=javax.jdo.annotations.IdGeneratorStrategy.IDENTITY,
-         column="id")
-@javax.jdo.annotations.Version(
-        strategy=VersionStrategy.VERSION_NUMBER, 
-        column="version")
+@javax.jdo.annotations.PersistenceCapable(identityType = IdentityType.DATASTORE)
+@javax.jdo.annotations.DatastoreIdentity(strategy = javax.jdo.annotations.IdGeneratorStrategy.IDENTITY, column = "id")
+@javax.jdo.annotations.Version(strategy = VersionStrategy.VERSION_NUMBER, column = "version")
 @ObjectType("ShiroUser")
 @Bounded
 public class ShiroUser {
-	
+
 	// {{ UserName (property)
 	private String userName;
 
@@ -42,8 +40,8 @@ public class ShiroUser {
 	public void setUserName(final String userName) {
 		this.userName = userName;
 	}
-	// }}
 
+	// }}
 
 	// {{ Password (property)
 	private String password;
@@ -57,16 +55,14 @@ public class ShiroUser {
 	public void setPassword(final String password) {
 		this.password = password;
 	}
+
 	// }}
-	
-	
+
 	// {{ RolesList (Collection)
 	@Join
 	@Element(dependent = "false")
-	
 	private SortedSet<Role> rolesList = new TreeSet<Role>();
 
-	
 	@MemberOrder(sequence = "3")
 	@Render(org.apache.isis.applib.annotation.Render.Type.EAGERLY)
 	public SortedSet<Role> getRolesList() {
@@ -76,49 +72,47 @@ public class ShiroUser {
 	public void setRolesList(final SortedSet<Role> rolesList) {
 		this.rolesList = rolesList;
 	}
+
 	// }}
 
-	//region > add role (action)
-    // //////////////////////////////////////
-    
-    @MemberOrder(sequence = "3")
-    @Named("Add Role for this User")    
-    public ShiroUser addRole(
-            final @Named("Role") Role role) {
-              
-        rolesList.add(role);
-        
-        return this;
-    }
+	// region > add role (action)
+	// //////////////////////////////////////
 
-    //endregion
+	@MemberOrder(sequence = "3")
+	@Named("Add Role for this User")
+	public ShiroUser addRole(final @Named("Role") Role role) {
 
-  //region > remove permission (action)
-    // //////////////////////////////////////
-    
-    @MemberOrder(sequence = "5")
-    
-    @Named("Remove Role for this User")
-    public ShiroUser removeRole(
-            final @Named("Role") Role role) {
-    	
-    	getRolesList().remove(role);    	
-        return this;
-    }
+		rolesList.add(role);
 
-    public SortedSet<Role> choices0RemoveRole(){
-    	return getRolesList();
-    }
-    
-    //endregion
+		return this;
+	}
 
+	// endregion
 
-    //region > injected services
-    // //////////////////////////////////////
+	// region > remove permission (action)
+	// //////////////////////////////////////
 
-    @javax.inject.Inject 
-    DomainObjectContainer container;
+	@MemberOrder(sequence = "5")
+	@Named("Remove Role for this User")
+	public ShiroUser removeRole(final @Named("Role") Role role) {
 
-    //endregion
+		getRolesList().remove(role);
+		return this;
+	}
+
+	public SortedSet<Role> choices0RemoveRole() {
+		return getRolesList();
+	}
+
+	// endregion
+
+		
+	// region > injected services
+	// //////////////////////////////////////
+
+	@javax.inject.Inject
+	DomainObjectContainer container;
+
+	// endregion
 
 }
--- dom/src/main/java/dom/simple/ShiroUserRepository.java
+++ dom/src/main/java/dom/simple/ShiroUserRepository.java
@@ -4,96 +4,115 @@ import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.isis.applib.DomainObjectContainer;
 import org.apache.isis.applib.annotation.ActionSemantics;
 import org.apache.isis.applib.annotation.Bookmarkable;
-import org.apache.isis.applib.annotation.Bounded;
 import org.apache.isis.applib.annotation.DomainService;
 import org.apache.isis.applib.annotation.Hidden;
 import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.annotation.Programmatic;
 import org.apache.isis.applib.annotation.ActionSemantics.Of;
 import org.apache.isis.applib.annotation.Where;
 
-
 @DomainService(menuOrder = "25", repositoryFor = ShiroUser.class)
 @Named("Shiro User")
-
 public class ShiroUserRepository {
-	
 
-    //region > identification in the UI
-    // //////////////////////////////////////
+	// region > identification in the UI
+	// //////////////////////////////////////
 
-    public String getId() {
-        return "shirouser";
-    }
+	public String getId() {
+		return "shirouser";
+	}
 
-    public String iconName() {
-        return "SimpleObject";
-    }
+	public String iconName() {
+		return "SimpleObject";
+	}
 
-    //endregion
+	// endregion
 
-    //region > listAll (action)
-    // //////////////////////////////////////
+	// region > listAll (action)
+	// //////////////////////////////////////
 
-    @Bookmarkable
-    @ActionSemantics(Of.SAFE)
-    @MemberOrder(sequence = "1")
-    @Named("List All Users")
-    public List<ShiroUser> listAll() {
-        return container.allInstances(ShiroUser.class);
-    }
+	@Bookmarkable
+	@ActionSemantics(Of.SAFE)
+	@MemberOrder(sequence = "1")
+	@Named("List All Users")
+	public List<ShiroUser> listAll() {
+		return container.allInstances(ShiroUser.class);
+	}
 
-    //endregion
+	// endregion
 
-    
-    //region > create (action)
-    // //////////////////////////////////////
-    
-    @MemberOrder(sequence = "2")
-    @Named("Create new User")
-    @Hidden(where = Where.OBJECT_FORMS)
-    public ShiroUser create(
-            final @Named("Name") String userName,
-            final @Named("Password") String password,
-            final @Named("Role") Role role) {
-        final ShiroUser obj = container.newTransientInstance(ShiroUser.class);
-        
-        final SortedSet<Role> rolesList = new TreeSet<Role>();
-        rolesList.add(role);
-        obj.setUserName(userName);
-        obj.setPassword(password);
-        obj.setRolesList(rolesList);
-        container.persistIfNotAlready(obj);
-        return obj;
-    }
+	// region > create (action)
+	// //////////////////////////////////////
 
-    //endregion
-    
-    
-    //region > remove User (action)
-    // //////////////////////////////////////
-    
-    @ActionSemantics(Of.NON_IDEMPOTENT)
-    @MemberOrder(sequence = "4")
-    @Named("Remove User")
-    public String removeUser(@Named("User") ShiroUser shiroUser) {
-    	String userName = shiroUser.getUserName();
-    	container.remove(shiroUser);
-        return "The user " + userName + " has been removed";
-    }
+	@MemberOrder(sequence = "2")
+	@Named("Create new User")
+	@Hidden(where = Where.OBJECT_FORMS)
+	public ShiroUser create(final @Named("Name") String userName,
+			final @Named("Password") String password,
+			final @Named("Role") Role role) {
+		final ShiroUser obj = container.newTransientInstance(ShiroUser.class);
 
-    //endregion
-  
-    //region > injected services
-    // //////////////////////////////////////
+		final SortedSet<Role> rolesList = new TreeSet<Role>();
+		rolesList.add(role);
+		obj.setUserName(userName);
+		obj.setPassword(password);
+		obj.setRolesList(rolesList);
+		container.persistIfNotAlready(obj);
+		return obj;
+	}
 
-    @javax.inject.Inject 
-    DomainObjectContainer container;
+	// endregion
 
-    //endregion
+	// region > remove User (action)
+	// //////////////////////////////////////
 
-	
+	@ActionSemantics(Of.NON_IDEMPOTENT)
+	@MemberOrder(sequence = "4")
+	@Named("Remove User")
+	public String removeUser(@Named("User") ShiroUser shiroUser) {
+		String userName = shiroUser.getUserName();
+		container.remove(shiroUser);
+		return "The user " + userName + " has been removed";
+	}
+
+	// endregion
+
+	// region > initialization
+	// //////////////////////////////////////
+
+	@Programmatic
+	@PostConstruct
+	public void init() {
+		List<ShiroUser> users = listAll();
+		if (users.isEmpty()) {
+			Permission permission = new Permission();
+			Role role = new Role();
+			SortedSet<Permission> permissions = new TreeSet<Permission>();
+
+			permission.setPermissionDescription("EVERYTHING");
+			permission.setPermissionText("*");
+			permissions.add(permission);
+			role.setRoleName("ADMINISTRATOR");
+			role.setPermissionsList(permissions);
+
+			create("sven", "pass", role);
+		}
+	}
+
+	// endregion
+
+	// region > injected services
+	// //////////////////////////////////////
+
+	@javax.inject.Inject
+	DomainObjectContainer container;
+
+	// endregion
+
 }
